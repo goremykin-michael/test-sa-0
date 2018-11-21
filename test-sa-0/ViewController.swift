4ed3017f4b12238b60c75dbbf66e7ba26f5b5be8
Michael Goremykin <goremykin.michael@gmail.com>
2018-11-22 01:51:54 +0400

    Initial commit.

---
+++ test-sa-0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test-sa-0
+//
+//  Created by Michael Goremykin on 22/11/2018.
+//  Copyright © 2018 Michael Goremykin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
